ccd506aa09997e44f055f327724c03016e8d5c63
Leonardo Deodônio da Silva <leonardodeodonio@gmail.com>
2022-11-02 16:42:14 -0300

    Primeiro-React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import StyledApp from "./components/StyledApp";
+import theme from "./config/theme/theme";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <StyledApp mode="dark">
+          <AppRoutes />
+        </StyledApp>
+      </div>
+    </ThemeProvider>
+     
+
+
+    );
 }
 
 export default App;
